77082010dc40e5c52c2e0a1692cdef706b8040be
katie <katya24081992@mail.ru>
2017-08-19 17:20:12 +0300

    ready

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/EkaterinaIV.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		let badges = document.getElementById('badges');
+  		for (i = 0; i < response.courses.completed.length; i++) {
+  			badges.innerHTML += "<div class='course'></div>";
+  		}
+  		let coursesDivs = document.getElementsByClassName('course');
+  		for (i = 0; i < coursesDivs.length; i++) {
+  			coursesDivs[i].innerHTML = '<h3>' + response.courses.completed[i].title + '</h3><img src="' + response.courses.completed[i].badge + '"><a target="_blank" class="btn btn-primary" href="' + response.courses.completed[i].url + '">See Course</a>';
+  		}
+  	}
+  })
 });
